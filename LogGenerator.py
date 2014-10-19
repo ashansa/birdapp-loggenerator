1a33d649d20dcd9d99ff494ed8027e3fb526b5da
shelan <shelanrc@gmail.com>
2014-10-19 15:57:23 +0100

    Adding files

---
+++ LogGenerator.py
@@ -0,0 +1,68 @@
+# This generate logs in the following format
+#
+#Sintra-1, 2013-10-09, 17:54.01, eagle-25, 3, 120, 0
+#
+# output file will be bird-log with log entries
+#
+#
+
+import time
+import random
+
+
+def read_input_files():
+    with open("input") as input_file:
+        for line in input_file:
+            line = line.rstrip()
+            tmp_split_vals = line.split(":")
+
+            if tmp_split_vals[0] == 'no_of_lines':
+                no_of_lines = tmp_split_vals[1]
+
+            if tmp_split_vals[0] == 'tower_ids':
+                tower_ids = tmp_split_vals[1].split(",")
+
+            if tmp_split_vals[0] == 'bird_ids':
+                bird_ids = tmp_split_vals[1].split(",")
+
+            input_file.close()
+
+        return no_of_lines, tower_ids, bird_ids
+
+
+def generate_random_date():
+    """
+    This function will return a random datetime between two datetime
+    objects.
+    """
+    format = '%Y-%m-%d %H:%M:%S'
+
+    startTime = int(time.time())
+    endTime = time.mktime(time.strptime("2013-01-01 00:00:00", format))
+
+    generatedTime = startTime + random.random() * (endTime - startTime)
+
+    return time.strftime(format, time.localtime(generatedTime))
+
+
+def generate_log():
+    """
+    sample log entry
+    Sintra-1, 2013-10-09, 17:54.01, eagle-25, 3, 120, 0.
+    :return:
+    """
+    logfile = open("bird-log", "w+");
+    for x in xrange(0, int(no_of_lines)):
+        randomDate = generate_random_date().split(" ");
+        print("generating line no : ", x)
+        logfile.writelines(random.choice(tower_ids) + ', ' + randomDate[0] + ', ' + randomDate[1] + ', '
+                           + random.choice(bird_ids) + ', ' + str(random.randint(1, 6)) + ', ' + str(
+            random.randint(5, 200)) + ', ' +
+                           str(random.randint(0, 1)) + "\n");
+
+    logfile.close()
+
+
+no_of_lines, tower_ids, bird_ids = read_input_files()
+
+generate_log()
